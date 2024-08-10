b61ca49061044e73c183feb16c4bfdfc2c3b720e
LamoureuseCB <148466239+LamoureuseCB@users.noreply.github.com>
2024-08-10 18:06:31 +0500

    new variant(teacher)

--- src/storage.sql
+++ src/storage.sql
@@ -1,4 +1,3 @@
-create database "catalog";
 create table category
 (
     id   serial primary key,
@@ -12,14 +11,19 @@ create table product
     manufacturer varchar(144) not null
 );
 
-create table product_characteristics
+create table option
 (
-    id                    serial primary key,
-    product_id            int references product (id),
-    characteristics_name  varchar(144) not null,
-    characteristics_value varchar(144) not null
+    id          serial primary key,
+    name        varchar(144) not null,
+    category_id int references category (id)
+);
 
-
+create table value
+(
+    id         serial primary key,
+    name       varchar(144) not null,
+    product_id int references product (id),
+    option_id  int references option (id)
 );
 
 insert into category (name)
@@ -32,25 +36,23 @@ values ('Intel Core I9 9900', 1, 'Intel'),
        ('Samsung SU556270', 2, 'Samsung'),
        ('AOC Z215S659', 2, 'AOC');
 
-insert into product_characteristics(product_id, characteristics_name, characteristics_value)
-values (1, 'Количество ядер', '8'),
-       (2, 'Количество ядер', '12'),
-       (1, 'Сокет', '1250'),
-       (2, 'Сокет', 'AM4'),
-       (3, 'Диагональ', '27'),
-       (4, 'Диагональ', '21.5'),
-       (3, 'Матрица', 'TN'),
-       (4, 'Матрица', 'AH-IPS'),
-       (3, 'Разрешение', '2560*1440'),
-       (4, 'Разрешение', '1920*1080');
+insert into option(name, category_id)
+values ('Количество ядер', 1),
+       ('Сокет', 1),
+       ('Диагональ', 2),
+       ('Матрица', 2),
+       ('Разрешение', 2);
 
-
-select product.name
-from product
-where product.id in (select product_characteristics.product_id
-                     from product_characteristics
-                     where product_characteristics.characteristics_name = 'Количество ядер');
-
-
+insert into value(name, option_id, product_id)
+values ('8', 1, 1),
+       ('1250', 2, 1),
+       ('12', 1, 2),
+       ('AM4', 2, 2),
+       ('27', 3, 3),
+       ('TN', 4, 3),
+       ('2560*1440', 5, 3),
+       ('21.5', 3, 4),
+       ('AH-IPS', 4, 4),
+       ('1920*1080', 5, 4);
 
 
